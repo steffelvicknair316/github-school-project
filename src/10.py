b4d874d852561df7307ecc82800364db31d1b80b
steffelvicknair316 <steffelvicknair316@gmail.com>
2025-03-15 22:39:30 +0000

    Add 10.py

---
+++ src/10.py
@@ -0,0 +1,10 @@
+def get_days_in_month(month):
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return 31
+    elif month == 4 or month == 6 or month == 9 or month == 11:
+        return 30
+    else:
+        if is_leap_year(year):
+            return 29
+        else:
+            return 28
