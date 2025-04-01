21118c13185f35aecaa9b9657896242f65c0e378
steffelvicknair316 <steffelvicknair316@gmail.com>
2025-04-01 15:58:24 +0000

    Add 24.py

---
+++ src/24.py
@@ -0,0 +1,15 @@
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+def subtract_numbers(a, b):
+    return a - b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def divide_numbers(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
