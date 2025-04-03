68bf24d7b8de9a064e2702c968046d96e09b1ad2
steffelvicknair316 <steffelvicknair316@gmail.com>
2025-04-03 13:35:34 +0000

    Add 26.py

---
+++ src/26.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(np.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
