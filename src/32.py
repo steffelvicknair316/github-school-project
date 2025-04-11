857aee25acd95ee9cec325c522b4a8eb1ed48b32
steffelvicknair316 <steffelvicknair316@gmail.com>
2025-04-11 11:32:56 +0000

    Add 32.py

---
+++ src/32.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def calculate_area(width, height):
+    """
+    Calculate the area of a rectangle.
+    
+    Parameters:
+    width (float): The width of the rectangle.
+    height (float): The height of the rectangle.
+    
+    Returns:
+    float: The area of the rectangle.
+    """
+    return width * height
