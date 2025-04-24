54965220c51799e15c37c7e54093fd563ed417b7
steffelvicknair316 <steffelvicknair316@gmail.com>
2025-04-24 15:59:54 +0000

    Add 35.py

---
+++ src/35.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data = {
+    'name': ['Alice', 'Bob', 'Charlie'],
+    'age': [18, 20, 19],
+    'country': ['USA', 'Canada', 'Australia']
+}
+df = pd.DataFrame(data)
+print(df)
